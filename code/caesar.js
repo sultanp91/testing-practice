ab78d7f10711709af4bb587c12b386bb76abb453
Sultan <sultanp@gmail.com>
2021-04-10 16:26:55 +0100

    caesar cipher function and tests

---
+++ code/caesar.js
@@ -0,0 +1,28 @@
+export default function caesarCipher(str, num) {
+    let newStr = "";
+    let lowerCaseStr = str.toLowerCase()
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    for(let i = 0; i < lowerCaseStr.length; i++){
+        const currentChar = lowerCaseStr[i]
+        if(currentChar === " " || alphabet.indexOf(currentChar) === -1){
+            newStr += currentChar;
+            continue;
+        }
+        const currentIndex = alphabet.indexOf(currentChar);
+        let cipherIndex = currentIndex + num
+        if(cipherIndex > 25){
+            cipherIndex = cipherIndex - 26
+        } 
+        else if (cipherIndex < 0){
+            cipherIndex = cipherIndex + 26
+        }
+        if(str[i] === str[i].toUpperCase()){
+            newStr += alphabet[cipherIndex].toUpperCase()
+        } else {
+            newStr += alphabet[cipherIndex]
+        }
+
+    }
+    return newStr;
+
+}
---
+++ tests/caesar.test.js
@@ -0,0 +1,22 @@
+import caesarCipher from '../code/caesar';
+import { expect } from '@jest/globals';
+
+test("a should change to c", () => {
+    expect(caesarCipher("a", 2)).toBe("c")
+})
+
+test("Wrapping: z should change to b", () => {
+    expect(caesarCipher("z", 2)).toBe("b")
+})
+
+test("Words: hello should change to Jgnnq", () => {
+    expect(caesarCipher("hello", 2)).toBe("jgnnq")
+})
+
+test("Capitalisation: Hello should change to Jgnnq", () => {
+    expect(caesarCipher("Hello", 2)).toBe("Jgnnq")
+})
+
+test("Punctuation should remain intact", () => {
+    expect(caesarCipher("Hello!", 2)).toBe("Jgnnq!")
+})
